5265355d380a30d398fe262e2899063a9b9ce61f
SajuAhmad <msahmad@mum.edu>
2019-08-06 20:24:18 -0500

    W3D2 done!

--- W3D2/js.js
+++ W3D2/js.js
@@ -1,30 +1,59 @@
 $(function () {
     "use strict";
 
-    let width = parseInt($("#width").val() ? $("#width").val() : 50);
-    console.log(width);
-    let amount = parseInt($("#growAmount").val() ? $("#growAmount").val() : 5);
-    console.log(amount);
-    let rate = parseInt($("#growRate").val() ? $("#growRate").val() : 10);
-    console.log(rate);
-
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
+    function generateColor() {
+        return '#' + ('000000' + Math.floor(Math.random() * 16777215).toString(16)).slice(-6);
+    }
+
+    // grow the size of circles
+    function growCircle() {
+        // read the values of input fields
+        let size = $("#growAmount").val();
+
+        // set default value of growth size
+        let growSize = parseInt(size ? size : 10);
+        $(".circle").each(function (idx, e) {
+            let element = $(e);
+            let curWidth = parseInt(element.css("width")) + growSize;
+            let curHeight = parseInt(element.css("height")) + growSize;
+            element
+                .css("width", curWidth + "px")
+                .css("height", curHeight + "px");
+        });
+    }
+
     function addCircle() {
-        //let pos = getRandomInt();
-        //console.log(pos);
-        $("#bubbleField").append($("<div>")
-            .addClass("circle")
-            .css("left", getRandomInt(1366) + "px"));
-    }
+        // read the values of input fields
+        let wdth = $("#width").val();
+        let growRate = $("#growRate").val();
+        let numOfCircles = $("#numOfCircle").val();
 
-    function createCircle() {
-        for (let i = 0; i < amount; i++) {
-            addCircle();
+        // set default value of width, growth rate and number of circles
+        let width = parseInt(wdth ? wdth : 50);
+        let rate = parseInt(growRate ? growRate : 250);
+        let circles = parseInt( numOfCircles? numOfCircles : 5);
+
+        // generate circles
+        for (let i = 0; i < circles; i++) {
+            $("#bubbleField").append($("<div>")
+                .addClass("circle")
+                .css("width", width + "px")
+                .css("height", width + "px")
+                .css("background-color", generateColor)
+                .css("left", getRandomInt(1366) + "px"));
         }
+        // grow the size of circles
+        let grow = setInterval(growCircle, rate);
     }
 
-    $("#settingBtn").on("click", createCircle);
+    function removeCircle() {
+        $(this).remove(); //jshint ignore:line
+    }
+
+    $("#settingBtn").on("click", addCircle);
+    $("#bubbleField").on("click", ".circle", removeCircle);
 });
